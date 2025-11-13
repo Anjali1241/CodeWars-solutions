37a866c29bdd9639a8d71a5edfa2cadb78881ffd
Anjali Sharma <78142594+Anjali1241@users.noreply.github.com>
2025-11-13 17:35:10 +0530

    Scramblies.jsx file added

---
+++ 5kyu_ Scramblies.jsx
@@ -0,0 +1,26 @@
+// Complete the function scramble(str1, str2) that returns true if a portion of str1 characters can be rearranged to match str2, otherwise returns false.
+
+// Notes:
+
+// Only lower case letters will be used (a-z). No punctuation or digits will be included.
+// Performance needs to be considered.
+// Examples
+// scramble('rkqodlw', 'world') ==> True
+// scramble('cedewaraaossoqqyt', 'codewars') ==> True
+// scramble('katas', 'steak') ==> False
+function scramble(str1, str2) {
+     
+    const count = {};
+for(let i=0;i<str1.length;i++){
+    count[str1[i]]=(count[str1[i]]||0)+1
+ 
+}
+
+for(let char of str2){
+        if (!count[char]) return false
+         count[char]--
+    
+}
+return true
+}
+
