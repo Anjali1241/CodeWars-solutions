438e5b8219a22054cf52992844ced2921dfe4dc9
Anjali Sharma <78142594+Anjali1241@users.noreply.github.com>
2026-01-02 17:06:59 +0530

    6 kyu (function currying)
    Implement a curried sum function that allows multiple additions.

---
+++ 6kyu/Sum1.jsx
@@ -0,0 +1,19 @@
+// PUZZLE #1. SUM
+// Assignment:
+
+// Write a function named sum which performs addition in the way shown below:
+
+// sum(4)(5)(9)(); // => 18
+// sum(5)();       // => 5
+// sum();          // => 0
+// NOTE: Pay attention that the last brackets are left empty to indicate the end of operations.
+
+function sum(a) {
+  let sum = a;
+
+  return function inner(b) {
+    if(b===undefined) return sum
+    sum += b;
+    return inner; 
+  }
+}
